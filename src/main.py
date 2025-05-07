04c61c25881ee6c2225f4ba13a9c946794c55a43
mahmoud radwan <medodiscordban@gmail.com>
2025-05-07 23:48:25 +0300

    untested more user-friendly

--- src/main.py
+++ src/main.py
@@ -1,29 +1,56 @@
 import os
 import time
 
-from instaloader import Instaloader, Profile
+import instaloader
 
-import config
+USERNAME = input("Enter your Username: ")
+PASSWORD = input("Enter your Password: ")
+PROFILE = input("Enter the Profile Username you want to watch: ")
+session = f"session-{USERNAME}"
 
-loader = Instaloader()
+loader = instaloader.Instaloader()
 
-if os.path.exists(f"session-{config.USERNAME}"):
-    loader.load_session_from_file(f"session-{config.USERNAME}")
+loader.context._session.headers.update({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+})
+
+if os.path.exists(session):
+    loader.load_session_from_file(username=USERNAME, filename=session)
 else:
-    loader.login(config.USERNAME, config.PASSWORD)
-    loader.save_session_to_file(filename=f"session-{config.USERNAME}")
+    try:
+        loader.login(USERNAME, PASSWORD)
+        loader.save_session_to_file(filename=f"{session}")
+    except instaloader.InstaloaderException as e:
+        print(f"Exception: {e}")
+        for i in range(5, 0, -1):
+            print(f"waiting {i} seconds", end="\r")
+            time.sleep(1)
+        exit(1)
 
-profile = Profile.from_username(loader.context, config.PROFILE)
+print("Waiting a Minute so we don't hit Instagram limit rate")
+time.sleep(60)
 
-followers = profile.get_followers()  # Saves the Original Followers Number.
+try:
+    profile = instaloader.Profile.from_username(loader.context, PROFILE)
+    followers = profile.get_followers()  # Saves the Original Followers Number.
+except instaloader.InstaloaderException as e:
+    print(f"Exception: {e}")
+    for i in range(5, 0, -1):
+        print(f"waiting {i} seconds", end="\r", flush=True)
+        time.sleep(1)
+    exit(1)
 
 while True:
-    current_followers = profile.get_followers()
+    try:
+        current_followers = profile.get_followers()
 
-    if len(current_followers) < len(followers):
-        unfollowers = list(set(followers) - set(current_followers))
-        print(f"User/s unfollowed you: {unfollowers}")
-    if len(current_followers) > len(followers):
-        followers = current_followers
+        if len(current_followers) < len(followers):
+            unfollowers = list(set(followers) - set(current_followers))
+            print(f"User/s unfollowed you: {unfollowers}")
+        if len(current_followers) > len(followers):
+            followers = current_followers
+
+    except instaloader.InstaloaderException as e:
+        print(f"Exception: {e}\n Trying again after 15 Mins")
 
     time.sleep(60 * 15)
